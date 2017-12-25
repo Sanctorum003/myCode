3e38a8db8cf22e20721c11f32cec326df1501490
Sanctorum003 <lushuchengsky@gmail.com>
2017-12-25 10:19:28 +0800

    Enter

--- problem_solve/temp.cpp
+++ problem_solve/temp.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-#include <stack>
+#include <cmath>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main(void)
 {
-    stack<int> s;
+    vector<int> v;
+    for(int i = 100; i>=0; --i)
+        v.push_back(1);
+
+    sort(v.begin(),v.end());
+
+    for(int i = 0; i < v.size()-1;++i)
+        cout<<v[i]<<endl;
+
+    cout<<endl;
+
     return 0;
 }
